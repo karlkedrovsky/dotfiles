1727c1f53bbad63c7b4e4ed42b1bae48bdea1672
Karl Kedrovsky <karl@kedrovsky.com>
2026-03-06 14:27:56 -0600

    added remaining config files from old directory

---
+++ unstowall.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+current_dir=$(pwd)
+
+cd "$(dirname "$0")"
+
+for package in */; do
+  # Remove the trailing slash from the package name
+  package=${package%/}
+  echo "Unstowing package: $package"
+  stow -D "$package"
+done
+
+cd $current_dir
